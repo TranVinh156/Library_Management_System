f1cefa69accfc838806c738091048c3a066ede6f
TranVinh156 <94176722+TranVinh156@users.noreply.github.com>
2024-11-05 13:17:00 +0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,8 +10,18 @@ module com.ooops.lms.library_management_system {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
-    requires java.sql;
     requires java.desktop;
+    requires annotations;
+    requires java.mail;
+    requires com.google.zxing.javase;
+    requires com.google.zxing;
+    requires opencv;
+    requires java.net.http;
+    requires com.google.gson;
+    requires java.sql;
+    requires mysql.connector.j;
+    requires org.json;
+
 
     opens com.ooops.lms.library_management_system to javafx.fxml;
     opens com.ooops.lms.controller to javafx.fxml;
