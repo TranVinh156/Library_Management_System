d1eb2938c4866b9af5380f1ab0b75055199ff626
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-07 04:43:56 +0700

    Create AdminUserPageController.java

---
+++ src/main/java/com/ooops/lms/controller/AdminUserPageController.java
@@ -0,0 +1,80 @@
+package com.ooops.lms.controller;
+
+import com.ooops.lms.Alter.CustomerAlter;
+import com.ooops.lms.model.Member;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class AdminUserPageController extends BasicUserController{
+
+    @FXML
+    private AnchorPane messageLocationPane;
+
+    @FXML
+    private AnchorPane userDetailLocationPane;
+
+    @FXML
+    private AnchorPane userTableLocationPane;
+
+    private AdminUserTableController adminUserTableController;
+    private AdminUserDetailController adminUserDetailController;
+
+    @FXML
+    public void initialize() throws IOException {
+        userTableLocationPane.getChildren().add(userTablePane);
+        adminUserTableController = userTablePaneLoader.getController();
+        adminUserTableController.setMainController(this);
+
+        messageLocationPane.getChildren().add(messagePane);
+
+        userDetailLocationPane.getChildren().add(userDetailPane);
+        adminUserDetailController = userDetailPaneLoader.getController();
+        adminUserDetailController.setMainController(this);
+
+    }
+
+    public void loadDetail(Member member) {
+        if(Objects.equals(adminUserDetailController.getMode(), "addMode")) {
+            boolean confirmYes = CustomerAlter.showAlter("Thông tin bạn đang thêm sẽ bị mất");
+            if(confirmYes) {
+                adminUserDetailController.loadStartStatus();
+                adminUserDetailController.setMember(member);
+            }
+        } else if(Objects.equals(adminUserDetailController.getMode(), "editMode")) {
+            if(adminUserDetailController.getMember().equals(member)) {
+                System.out.println("van mot nguoi ma dcm");
+            } else {
+                boolean confirmYes = CustomerAlter.showAlter("Thông tin bạn đang sửa sẽ bị mất");
+                if(confirmYes) {
+                    adminUserDetailController.loadStartStatus();
+                    adminUserDetailController.setMember(member);
+                }
+            }
+        } else if(Objects.equals(adminUserDetailController.getMode(), "noneMode")) {
+            adminUserDetailController.loadStartStatus();
+            adminUserDetailController.setMember(member);
+        } else {
+            adminUserDetailController.setMember(member);
+        }
+    }
+
+    public void loadAddPane() {
+        if(Objects.equals(adminUserDetailController.getMode(), "editMode")) {
+            boolean confirmYes = CustomerAlter.showAlter("Thông tin bạn đang sửa sẽ bị mất");
+            if(confirmYes) {
+                adminUserDetailController.loadStartStatus();
+                adminUserDetailController.setAddMode(true);
+            }
+        } else if(Objects.equals(adminUserDetailController.getMode(), "noneMode")) {
+            adminUserDetailController.setAddMode(true);
+        }
+    }
+
+    public void registerNewMember(Member member) {
+        adminUserTableController.addMember(member);
+    }
+
+}
