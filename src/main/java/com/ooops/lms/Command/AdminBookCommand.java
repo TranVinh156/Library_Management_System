c94e1fed18ad0bb259cd9d59868f4699e9141eac
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-07 04:43:18 +0700

    Create AdminBookDetailController.java

---
+++ src/main/java/com/ooops/lms/Command/AdminBookCommand.java
@@ -0,0 +1,25 @@
+package com.ooops.lms.Command;
+
+public class AdminBookCommand implements Command {
+    private String action;
+    private Object data;
+
+    public AdminBookCommand(String action, Object data) {
+        this.action = action;
+        this.data = data;
+    }
+
+    @Override
+    public void execute() {
+        switch (action) {
+            case "add":
+                break;
+            case "delete":
+                break;
+            case "edit":
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ src/main/java/com/ooops/lms/controller/AdminBookDetailController.java
@@ -0,0 +1,290 @@
+package com.ooops.lms.controller;
+
+import com.ooops.lms.Alter.CustomerAlter;
+import com.ooops.lms.model.Book;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class AdminBookDetailController extends BasicBookController {
+
+    @FXML
+    private TextField ISBNText;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private AnchorPane addButtonPane;
+
+    @FXML
+    private TextField authorNameText;
+
+    @FXML
+    private TextArea bookContentText;
+
+    @FXML
+    private ImageView bookImage;
+
+    @FXML
+    private TextField bookNameText;
+
+    @FXML
+    private TextField categoryText;
+
+    @FXML
+    private AnchorPane copyBookPane;
+
+    @FXML
+    private VBox copyBookTableVbox;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button ediButton;
+
+    @FXML
+    private TextField locationText;
+
+    @FXML
+    private TextField numberOfBookText;
+
+    @FXML
+    private TextField numberOfBorrowText;
+
+    @FXML
+    private TextField numberOfLostText;
+
+    @FXML
+    private Button page1Button;
+
+    @FXML
+    private Button page2Button;
+
+    @FXML
+    private TextField publishingHouseText;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button scanButton;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    private AdminBookPageController mainController;
+
+    private Book book;
+
+    private boolean editMode = false;
+    private boolean addMode = false;
+    private boolean isPage1 = true;
+
+    public void setMainController(AdminBookPageController mainController) {
+        this.mainController = mainController;
+    }
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    void onAddButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEditButtonAction(ActionEvent event) {
+        if (book != null) {
+            setEditMode(true);
+        }
+    }
+
+    @FXML
+    void onPage1ButtonAction(ActionEvent event) {
+        System.out.println("Page 1");
+        isPage1 = true;
+        setButtonPageAnimation();
+        openPagePane();
+    }
+
+    @FXML
+    void onPage2ButtonAction(ActionEvent event) {
+        System.out.println("Page 2");
+        isPage1 = false;
+        setButtonPageAnimation();
+        openPagePane();
+    }
+
+    @FXML
+    void onSaveButtonAction(ActionEvent event) {
+        if (addMode) {
+            if(getItemInfomation()) {
+                boolean confirmYes = CustomerAlter.showAlter("Lưu?");
+                if (confirmYes) {
+                    //mainController.registerNewItem(book);
+                    System.out.println("Đã lưu thay đổi");
+                    setAddMode(false);
+                }
+            }
+            else {
+                System.out.println("Tiếp tục edit");
+            }
+        } else {
+            boolean confirmYes = CustomerAlter.showAlter("Bạn có muốn lưu thay đổi này không?");
+            if (confirmYes) {
+                //nhớ thêm hàm save thông tin ở đây
+                setEditMode(false);
+                System.out.println("Đã lưu thay đổi");
+            } else {
+                System.out.println("Tiếp tục edit");
+            }
+        }
+    }
+
+    private boolean getItemInfomation() {
+        book.setTitle(bookNameText.getText());
+        //book.setAuthors(authorNameText.getText());
+        //book.setISBN(ISBNText.getText());
+        book.setDescription(bookContentText.getText());
+        book.setPlaceAt(locationText.getText());
+
+        if(checkInformation(book)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean checkInformation(Book book) {
+        return true;
+    }
+
+    @FXML
+    void onScanButtonAction(ActionEvent event) {
+
+    }
+
+    public void setItem(Book book) {
+        this.book = book;
+
+        bookNameText.setText(book.getTitle());
+        ISBNText.setText(String.valueOf(book.getISBN()));
+        authorNameText.setText(getAuthors(book.getAuthors()));
+        categoryText.setText(getCategories(book.getCategories()));
+        publishingHouseText.setText("chua co");
+        numberOfBookText.setText("khong co");
+        numberOfBorrowText.setText(String.valueOf(book.getNumberOfLoanedBooks()));
+        numberOfLostText.setText(String.valueOf(book.getNumberOfLostBooks()));
+        locationText.setText(book.getPlaceAt());
+        bookContentText.setText(book.getDescription());
+    }
+
+    public Book getItem() {
+        return this.book;
+    }
+
+    public String getMode() {
+        if(addMode) {
+            return "addMode";
+        } else if(editMode) {
+            return "editMode";
+        } else {
+            return "noneMode";
+        }
+    }
+
+    private void setEditMode(boolean edit) {
+        editMode = edit;
+        ediButton.setVisible(!edit);
+        deleteButton.setVisible(edit);
+        saveButton.setVisible(edit);
+        //permit for edit
+        ISBNText.setEditable(edit);
+        bookNameText.setEditable(edit);
+        locationText.setEditable(edit);
+        authorNameText.setEditable(edit);
+        categoryText.setEditable(edit);
+        publishingHouseText.setEditable(edit);
+        numberOfBookText.setEditable(edit);
+
+    }
+
+    private void setAddMode(boolean add) {
+        addMode = add;
+        ediButton.setVisible(!add);
+        deleteButton.setVisible(add);
+        saveButton.setVisible(add);
+
+        //permit for edit
+        ISBNText.setEditable(add);
+        bookNameText.setEditable(add);
+        locationText.setEditable(add);
+        authorNameText.setEditable(add);
+        categoryText.setEditable(add);
+        publishingHouseText.setEditable(add);
+        numberOfBookText.setEditable(add);
+        if(addMode) {
+            ISBNText.setText(null);
+            bookNameText.setText(null);
+            locationText.setText(null);
+            authorNameText.setText(null);
+            categoryText.setText(null);
+            publishingHouseText.setText(null);
+            numberOfBookText.setText(null);
+        }
+
+    }
+
+    public void loadStartStatus() {
+        book = new Book();
+        setAddMode(false);
+        setEditMode(false);
+    }
+
+    private void openPagePane() {
+        copyBookPane.setVisible(!isPage1);
+    }
+
+    private void setButtonPageAnimation() {
+        if(isPage1) {
+            //choice button color darker
+            page1Button.setStyle("-fx-background-color: #DDDCDC;" );
+            page2Button.setStyle("-fx-background-color: #FFF;");
+
+            //choice button bring to front
+            if (page1Button.getParent().getChildrenUnmodifiable().indexOf(page1Button) <
+                    page1Button.getParent().getChildrenUnmodifiable().size() - 1) {
+                page2Button.toBack();
+            }
+        } else {
+            //choice button color darker
+            page2Button.setStyle("-fx-background-color: #DDDCDC;");
+            page1Button.setStyle("-fx-background-color: #FFF;");
+
+            //choice button bring to front
+            if (page2Button.getParent().getChildrenUnmodifiable().indexOf(page2Button) <
+                    page2Button.getParent().getChildrenUnmodifiable().indexOf(page1Button)) {
+                page1Button.toBack();
+            }
+        }
+    }
+
+
+
+
+}
