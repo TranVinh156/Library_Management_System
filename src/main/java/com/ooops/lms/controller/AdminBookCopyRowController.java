3a97fcc361b2bd9a62efa5969151717d43905716
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-07 04:48:46 +0700

    Create AdminBookCopyRowController.java

---
+++ src/main/java/com/ooops/lms/controller/AdminBookCopyRowController.java
@@ -0,0 +1,27 @@
+package com.ooops.lms.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AdminBookCopyRowController {
+
+    @FXML
+    private TextField barcodeText;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private TextField noteText;
+
+    @FXML
+    private TextField statusText;
+
+    @FXML
+    void onEditButtonAction(ActionEvent event) {
+
+    }
+
+}
