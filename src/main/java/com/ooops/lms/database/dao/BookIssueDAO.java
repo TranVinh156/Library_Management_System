955b9dc21b642182ecec5b2c236af45d9dd8dc62
TranVinh156 <94176722+TranVinh156@users.noreply.github.com>
2024-11-07 13:19:16 +0700

    Update BookIssueDAO.java

--- src/main/java/com/ooops/lms/database/dao/BookIssueDAO.java
+++ src/main/java/com/ooops/lms/database/dao/BookIssueDAO.java
@@ -1,43 +1,45 @@
 package com.ooops.lms.database.dao;
 
 import com.ooops.lms.database.Database;
+import com.ooops.lms.model.BookIssue;
 import com.ooops.lms.model.enums.BookItemStatus;
 
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
-public class BookIssueDAO implements DatabaseQuery {
+public class BookIssueDAO implements DatabaseQuery<BookIssue> {
     private Database database;
     private BookItemStatus status;
 
+
     @Override
-    public void add(Object entity) throws SQLException {
+    public void add(BookIssue entity) throws SQLException {
 
     }
 
     @Override
-    public boolean update(Object entity) throws SQLException {
+    public boolean update(BookIssue entity) throws SQLException {
         return false;
     }
 
     @Override
-    public boolean delete(Object entity) throws SQLException {
+    public boolean delete(BookIssue entity) throws SQLException {
         return false;
     }
 
     @Override
-    public Object find(Number keywords) throws SQLException {
+    public BookIssue find(Number keywords) throws SQLException {
         return null;
     }
 
     @Override
-    public List selectAll() throws SQLException {
+    public List<BookIssue> searchByCriteria(Map<String, Object> criteria) throws SQLException {
         return List.of();
     }
 
     @Override
-    public List searchByCriteria(Map criteria) throws SQLException {
+    public List<BookIssue> selectAll() throws SQLException {
         return List.of();
     }
 }
