1063ccf0aacdad2a7ac391e3d994810a5a06c4c9
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-07 13:17:46 +0700

    Create ResignCommand.java

---
+++ src/main/java/com/ooops/lms/Command/ResignCommand.java
@@ -0,0 +1,17 @@
+package com.ooops.lms.Command;
+
+import javafx.stage.Stage;
+
+public class ResignCommand implements Command {
+    private String role;
+    private Stage stage;
+
+    public ResignCommand(String role, Stage stage) {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
