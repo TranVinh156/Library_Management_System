3e8fc2d613e5a4ebe5e1f524fede27e5a449034a
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-09 01:48:01 +0700

    Update AdminUserDetailController.java

--- src/main/java/com/ooops/lms/controller/AdminUserDetailController.java
+++ src/main/java/com/ooops/lms/controller/AdminUserDetailController.java
@@ -3,12 +3,10 @@ package com.ooops.lms.controller;
 import com.ooops.lms.Alter.CustomerAlter;
 import com.ooops.lms.Command.AdminCommand;
 import com.ooops.lms.Command.Command;
-import com.ooops.lms.model.Admin;
 import com.ooops.lms.model.Member;
 import com.ooops.lms.model.datatype.Person;
 import com.ooops.lms.model.enums.AccountStatus;
 import com.ooops.lms.model.enums.Gender;
-import com.ooops.lms.model.enums.MemberStatus;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -72,13 +70,19 @@ public class AdminUserDetailController extends BasicUserController {
     @FXML
     private ImageView userImage;
 
-    private Member member;
-    private Person person = new Person();
-    private String imagePath;
-
     private AdminUserPageController mainController;
     private boolean addMode = false;
     private boolean editMode = false;
+    private Member member;
+
+    /**
+     * Hàm để set mainController (PageController).
+     *
+     * @param controller là PageController truyền vào
+     */
+    public void setMainController(AdminUserPageController controller) {
+        this.mainController = controller;
+    }
 
     @FXML
     void initialize() {
@@ -88,21 +92,40 @@ public class AdminUserDetailController extends BasicUserController {
 
     @FXML
     void onBlockButtonAction(ActionEvent event) {
-        boolean confirmYes = CustomerAlter.showAlter("Bạn block người dùng này á?");
-        if (confirmYes) {
-            boolean confirmYes2 = CustomerAlter.showAlter("Bạn block thật đấy à?");
-            if (confirmYes2) {
-                Admin.blockMember(member);
-                reloadMemberStatus();
+        if(member.getStatus().equals(AccountStatus.ACTIVE)) {
+            boolean confirmYes = CustomerAlter.showAlter("Bạn block người đọc này á?");
+            if (confirmYes) {
+                boolean confirmYes2 = CustomerAlter.showAlter("Bạn block thật đấy à?");
+                if (confirmYes2) {
+                    Command blockCommand = new AdminCommand("block",this.member);
+                    commandInvoker.setCommand(blockCommand);
+                    if(commandInvoker.executeCommand()) {
+                        statusBox.getSelectionModel().select(AccountStatus.BLOCKED);
+                        System.out.println("Đã block người đọc");
+                    }
+                }
+            }
+        } else if(member.getStatus().equals(AccountStatus.BLOCKED)) {
+            boolean confirmYes = CustomerAlter.showAlter("Bạn unblock người đọc này?");
+            if (confirmYes) {
+                Command unblockCommand = new AdminCommand("unblock",this.member);
+                commandInvoker.setCommand(unblockCommand);
+                if(commandInvoker.executeCommand()) {
+                    statusBox.getSelectionModel().select(AccountStatus.ACTIVE);
+                    System.out.println("Đã unblock người đọc");
+                }
             }
         }
     }
 
+    //Nút chọn ảnh
     @FXML
     void onChoiceImageButtonAction(ActionEvent event) {
-        person.setImagePath(getImagePath());
-        if (person.getImagePath() != null) {
-            Image image = new Image(person.getImagePath());
+        member.getPerson().setImagePath(getImagePath());
+
+        // Xử lý nếu như có chọn ảnh thì trường avatar của người dùng hiện ảnh
+        if (member.getPerson().getImagePath() != null) {
+            Image image = new Image(member.getPerson().getImagePath());
             userImage.setImage(image);
         }
     }
@@ -114,75 +137,24 @@ public class AdminUserDetailController extends BasicUserController {
         }
     }
 
-    private void setEditMode(boolean edit) {
-        editMode = edit;
-        editButton.setVisible(!edit);
-        choiceImageButton.setVisible(edit);
-        saveButton.setVisible(edit);
-        saveButton.setText("Save");
-        deleteButton.setVisible(edit);
-        blockMember.setVisible(edit);
-
-        emailText.setEditable(edit);
-        genderBox.setMouseTransparent(!edit);
-        memberNameText.setEditable(edit);
-        phoneNumberText.setEditable(edit);
-        resignDateText.setEditable(edit);
-        birthText.setEditable(edit);
-        if (member != null && member.getPerson() != null && member.getPerson().getImagePath() != null) {
-            userImage.setImage(new Image(member.getPerson().getImagePath()));
-        } else {
-            userImage.setImage(defaultUserImage);
-        }
-
-    }
-
-    public void setAddMode(boolean add) {
-        addMode = add;
-
-        editButton.setVisible(!add);
-        choiceImageButton.setVisible(add);
-        saveButton.setVisible(add);
-
-        emailText.setEditable(add);
-        genderBox.setMouseTransparent(!add);
-        memberNameText.setEditable(add);
-        phoneNumberText.setEditable(add);
-        resignDateText.setEditable(add);
-        birthText.setEditable(add);
-        saveButton.setText("Add");
-        if (addMode) {
-            userImage.setImage(defaultUserImage);
-            emailText.setText(null);
-            genderBox.setValue(null);
-            birthText.setText(null);
-            memberIDText.setText(null);
-            memberNameText.setText(null);
-            numberOfBorrowText.setText(null);
-            numberOfLostText.setText(null);
-            phoneNumberText.setText(null);
-            resignDateText.setText(LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yy")));
-            statusBox.setValue(AccountStatus.ACTIVE);
-        } else {
-            if(member != null) {
-                memberIDText.setText(String.valueOf(member.getPerson().getId()));
-            }
-        }
-    }
-
+    /**
+     * Save button dùng chung cho cả AddMode và EditMode.
+     *
+     * @param event
+     */
     @FXML
     void onSaveButtonAction(ActionEvent event) {
         if (addMode) {
             if (getNewMemberInfomation()) {
-                boolean confirmYes = CustomerAlter.showAlter("Thêm bạn đọc mới?");
+                boolean confirmYes = CustomerAlter.showAlter("Thêm bạn đọc mới?"); //Nếu như thông tin của member đã hợp lệ để nhập vào CSDL
                 if (confirmYes) {
                     //thêm người dùng vào CSDL
                     Command addCommand = new AdminCommand("add", this.member);
                     commandInvoker.setCommand(addCommand);
                     if (commandInvoker.executeCommand()) {
-                        mainController.registerNewMember(member);
-                        System.out.println("Đã lưu thay đổi");
+                        mainController.loadData(); // cập nhật lại UserTable
                         setAddMode(false);
+                        System.out.println("Đã lưu thay đổi");
                     }
                 }
             } else {
@@ -192,12 +164,11 @@ public class AdminUserDetailController extends BasicUserController {
             boolean confirmYes = CustomerAlter.showAlter("Bạn có muốn lưu thay đổi này không?");
             if (getNewMemberInfomation()) {
                 if (confirmYes) {
-                    //nhớ thêm hàm save thông tin ở đây
-
+                    // sửa người dùng trong CSDL
                     Command editCommand = new AdminCommand("edit", this.member);
                     commandInvoker.setCommand(editCommand);
-
                     if (commandInvoker.executeCommand()) {
+                        mainController.loadData(); // cập nhật lại UserTable
                         setEditMode(false);
                         System.out.println("Đã lưu thay đổi");
                     }
@@ -213,16 +184,93 @@ public class AdminUserDetailController extends BasicUserController {
     void onDeleteButtonAction(ActionEvent event) {
         boolean confrimYes = CustomerAlter.showAlter("Bạn muốn xóa người dùng này?");
         if (confrimYes) {
-
+            Command deleteCommand = new AdminCommand("delete", this.member);
+            commandInvoker.setCommand(deleteCommand);
+            if (commandInvoker.executeCommand()) {
+                mainController.loadData();
+                loadStartStatus();
+                System.out.println("Đã xóa người dùng");
+            } else {
+                CustomerAlter.showMessage("Xóa người dùng thất bại");
+            }
+        } else {
+            System.out.println("Tiếp tục edit");
         }
     }
 
-    public void setMainController(AdminUserPageController controller) {
-        this.mainController = controller;
+    /**
+     * Set edit Mode.
+     *
+     * @param edit true nếu là bật, false nếu tắt.
+     */
+    private void setEditMode(boolean edit) {
+        editMode = edit;
+        //Xử lý các nút ẩn hiện
+        editButton.setVisible(!edit);
+        choiceImageButton.setVisible(edit);
+        saveButton.setVisible(edit);
+        saveButton.setText("Save");
+        deleteButton.setVisible(edit);
+        blockMember.setVisible(edit);
+
+        //Cho phép người dùng sửa các trường
+        emailText.setEditable(edit);
+        genderBox.setMouseTransparent(!edit);
+        memberNameText.setEditable(edit);
+        phoneNumberText.setEditable(edit);
+        resignDateText.setEditable(edit);
+        birthText.setEditable(edit);
+        statusBox.setMouseTransparent(!edit);
+
     }
 
+    /**
+     * Set add Mode.
+     *
+     * @param add true nếu bật, false nếu tắt.
+     */
+    public void setAddMode(boolean add) {
+        addMode = add;
+        // Xử lý ẩn hiện các nút
+        editButton.setVisible(!add);
+        choiceImageButton.setVisible(add);
+        saveButton.setVisible(add);
+        saveButton.setText("Add");
+
+        //Cho phép người dùng nhập các trường
+        emailText.setEditable(add);
+        genderBox.setMouseTransparent(!add);
+        memberNameText.setEditable(add);
+        phoneNumberText.setEditable(add);
+        resignDateText.setEditable(add);
+        birthText.setEditable(add);
+        statusBox.setMouseTransparent(!add);
+
+        //Nếu như mà là mở addMode thì các trường sẽ rỗng (addMode == true)
+        if (addMode) {
+            userImage.setImage(defaultUserImage);
+            emailText.setText(null);
+            genderBox.setValue(null);
+            birthText.setText(null);
+            memberIDText.setText(null);
+            memberNameText.setText(null);
+            numberOfBorrowText.setText(null);
+            numberOfLostText.setText(null);
+            phoneNumberText.setText(null);
+            resignDateText.setText(LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yy")));
+            statusBox.setValue(AccountStatus.ACTIVE);
+        }
+    }
+
+    /**
+     * Hàm dùng để lấy thông tin từ Member và gán thông tin cho các trường.
+     *
+     * @param member gán thông tin
+     */
     public void setMember(Member member) {
         this.member = member;
+
+        // Gán thông tin cho các trường
         emailText.setText(member.getPerson().getEmail());
         genderBox.setValue(member.getPerson().getGender());
         memberIDText.setText(String.valueOf(member.getPerson().getId()));
@@ -230,50 +278,72 @@ public class AdminUserDetailController extends BasicUserController {
         numberOfBorrowText.setText(String.valueOf(member.getTotalBooksCheckOut()));
         numberOfLostText.setText("Chua co");
         phoneNumberText.setText(member.getPerson().getPhone());
-        birthText.setText(member.getPerson().getDateOfBirth().toString());
-        resignDateText.setText(member.getCreatedDate());
+        birthText.setText(reformatDate(member.getPerson().getDateOfBirth().toString()));
         statusBox.setValue(member.getStatus());
+
+        //Xử lý ngày tháng đăng ký
+        String resignDate = member.getCreatedDate().toString();
+        String formatResignDate = reformatDate(resignDate);
+        resignDateText.setText(formatResignDate);
+
+        // Nếu như ảnh của member mà không có hoặc đường dẫn ảnh lỗi thì set mặc định
         if (member.getPerson().getImagePath() != null && isValidImagePath(member.getPerson().getImagePath())) {
             userImage.setImage(new Image(member.getPerson().getImagePath()));
         } else {
             userImage.setImage(defaultUserImage);
         }
-
     }
 
+    /**
+     * Hàm lấy thông tin để truyền vào member của fxml này.
+     * Kết hợp hàm kiểm tra thông tin để biết thông tin người dùng đã hợp lệ chưa.
+     *
+     * @return true/false
+     */
     public boolean getNewMemberInfomation() {
+        // Xử lý firstName và lastName
         String memberName = memberNameText.getText();
         if (memberName != null) {
             String[] nameParts = memberName.trim().split("\\s+");
-            person.setFirstName(nameParts[0]); // Tên đầu tiên
-            person.setLastName(nameParts.length > 1 ? nameParts[nameParts.length - 1] : "");
+            member.getPerson().setFirstName(nameParts[0]); // Tên đầu tiên
+            member.getPerson().setLastName(nameParts.length > 1 ? nameParts[nameParts.length - 1] : "");
         } else {
-            person.setFirstName(null);
-            person.setLastName(null);
+            member.getPerson().setFirstName(null);
+            member.getPerson().setLastName(null);
         }
-        person.setEmail(emailText.getText());
-        person.setGender(genderBox.getValue());
-        person.setPhone(phoneNumberText.getText());
-        person.setGender(genderBox.getValue());
+
+        // Lấy các thông tin khác.
+        member.getPerson().setEmail(emailText.getText());
+        member.getPerson().setGender(genderBox.getValue());
+        member.getPerson().setPhone(phoneNumberText.getText());
+        member.getPerson().setGender(genderBox.getValue());
+        member.setStatus(statusBox.getValue());
 
         // đổi kiểu ngay từ dd/MM/yyyy thành yyyy-MM-dd
         String birthDate = birthText.getText();
         String reformattedDate = reformatDate(birthDate);
-        person.setDateOfBirth(reformattedDate);
+        member.getPerson().setDateOfBirth(reformattedDate);
 
-        if (checkInformation(person)) {
-            if (person.getImagePath() == null) {
-                person.setImagePath(Person.DEFAULT_IMAGE_PATH);
-            }
-            this.member = new Member(null, null, person);
-            member.setStatus(statusBox.getValue());
-            return true;
+        //nếu như person không có ảnh thì cho ảnh mặc định
+        if (member.getPerson().getImagePath() == null) {
+            member.getPerson().setImagePath(Person.DEFAULT_IMAGE_PATH);
+        }
+
+        //Kiểm tra các thông tin của person đã hợp lệ hay chưa
+        if (checkInformation(member.getPerson())) {
+            return true; // đã có thể đẩy thông tin person vào CSDL
         } else {
-            return false;
+            return false; // chưa thể đẩy thông tin person vào CSDL
         }
 
     }
 
+    /**
+     * Hàm kiểm tra thông tin của Person đã đúng chưa trước khi nhập vào CSDL.
+     *
+     * @param person là person cần kiểm tra thông tin
+     * @return true/false
+     */
     private boolean checkInformation(Person person) {
         // Kiểm tra xem tên không được rỗng
         if (person.getFirstName() == null || person.getFirstName().isEmpty()) {
@@ -311,23 +381,13 @@ public class AdminUserDetailController extends BasicUserController {
 
     }
 
-    public static String reformatDate(String dateStr) {
-        // Định dạng ban đầu
-        SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy");
-        // Định dạng mong muốn
-        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-        try {
-            // Chuyển chuỗi thành đối tượng Date theo định dạng đầu vào
-            Date date = inputFormat.parse(dateStr);
-            // Định dạng lại đối tượng Date theo định dạng đầu ra
-            return outputFormat.format(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return null; // Trả về null nếu có lỗi
-        }
-    }
-
+    /**
+     * Hàm kiểm tra tính hợp lệ của ngày tháng năm.
+     *
+     * @param dateStr ngày tháng cần kiểm tra
+     * @return true/false
+     */
     private boolean isValidDate(String dateStr) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         dateFormat.setLenient(false); // Không cho phép ngày không hợp lệ
@@ -348,21 +408,66 @@ public class AdminUserDetailController extends BasicUserController {
         return true;
     }
 
+
+    /**
+     * Hàm chuyển đổi format cho ngày tháng năm.
+     * Nếu là định dạng dd/MM/yyyy thì chuyển sang yyyy-MM-dd.
+     * Nếu là định dạng yyyy-MM-dd thì chuyển sang định dạng dd/MM/yyyy.
+     *
+     * @param dateStr ngày tháng năm cần chuyển đổi
+     * @return ngày tháng năm đã dược chuyển đổi
+     */
+    private static String reformatDate(String dateStr) {
+        SimpleDateFormat format1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            // Thử parse theo định dạng dd/MM/yyyy
+            try {
+                Date date = format1.parse(dateStr);
+                return format2.format(date); // Chuyển sang yyyy-MM-dd
+            } catch (ParseException e1) {
+                // Nếu không được, thử parse theo định dạng yyyy-MM-dd
+                Date date = format2.parse(dateStr);
+                return format1.format(date); // Chuyển sang dd/MM/yyyy
+            }
+        } catch (ParseException e2) {
+            // Nếu cả hai định dạng đều không đúng
+            return null;
+        }
+    }
+
+
+    /**
+     * Hàm để load bảng Detail về trạng thái rỗng ban đầu.
+     */
     public void loadStartStatus() {
-        person = new Person();
-        member = new Member(null, null, null);
+        //Set member của bảng Detail rỗng
+        member = null;
+
+        // Set các trường nhập thành rỗng
+        userImage.setImage(null);
+        emailText.setText(null);
+        genderBox.setValue(null);
+        birthText.setText(null);
+        memberIDText.setText(null);
+        memberNameText.setText(null);
+        numberOfBorrowText.setText(null);
+        numberOfLostText.setText(null);
+        phoneNumberText.setText(null);
+        resignDateText.setText(null);
+        statusBox.setValue(null);
+
+        // Set trạng thái bảng về noneMode
         setAddMode(false);
         setEditMode(false);
     }
 
-    public Member getMember() {
-        return member;
-    }
-
-    private void reloadMemberStatus() {
-
-    }
-
+    /**
+     * Hàm để lấy trạng thái của bảng Detail (add/edit/none)
+     *
+     * @return addMode/editMode/noneMode
+     */
     public String getMode() {
         if (addMode) {
             return "addMode";
@@ -371,5 +476,10 @@ public class AdminUserDetailController extends BasicUserController {
         } else {
             return "noneMode";
         }
+    }
+
+
+    public Member getMember() {
+        return this.member;
     }
 }
