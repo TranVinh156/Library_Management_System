5d5e35c5a43bc5c98c2ecd4097c31ef1c01bd26e
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-09 03:50:55 +0700

    Update AdminBookPageController.java

--- src/main/java/com/ooops/lms/controller/AdminBookPageController.java
+++ src/main/java/com/ooops/lms/controller/AdminBookPageController.java
@@ -41,13 +41,22 @@ public class AdminBookPageController extends BasicBookController {
     }
 
     public void loadDetail(Book book) {
-        System.out.println("load Detail");
         alterPage();
     }
 
-    public void alterPage() {
+    public void loadAddPane() {
+        adminBookDetailController.loadStartStatus();
+        adminBookDetailController.setAddMode(true);
+        alterPage();
+    }
+
+    private void alterPage() {
         detailPage.setVisible(!detailPage.isVisible());
         tablePage.setVisible(!tablePage.isVisible());
+    }
+
+    public void loadData() {
+
     }
 
 }
