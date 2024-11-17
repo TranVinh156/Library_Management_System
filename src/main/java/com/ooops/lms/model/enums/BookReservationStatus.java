4342a25fe98b7ea9e46af36939d7412269951752
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-17 22:35:40 +0700

    Update BookReservationStatus.java

--- src/main/java/com/ooops/lms/model/enums/BookReservationStatus.java
+++ src/main/java/com/ooops/lms/model/enums/BookReservationStatus.java
@@ -1,7 +1,23 @@
 package com.ooops.lms.model.enums;
 
 public enum BookReservationStatus {
-    WAITING,
-    COMPLETED,
-    CANCELED
+    WAITING("Đang chờ"),
+    COMPLETED("Đã xong"),
+    CANCELED("Đã hủy");
+
+    private final String displayName;
+
+    BookReservationStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName; // Sử dụng tên hiển thị thay vì tên enum
+    }
+
 }
