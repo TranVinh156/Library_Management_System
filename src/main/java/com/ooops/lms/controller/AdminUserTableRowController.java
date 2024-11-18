445f5384c28a255aed4c486feb18d2bc37c399f6
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-18 14:28:27 +0700

    Update AdminUserTableRowController.java

--- src/main/java/com/ooops/lms/controller/AdminUserTableRowController.java
+++ src/main/java/com/ooops/lms/controller/AdminUserTableRowController.java
@@ -1,11 +1,13 @@
 package com.ooops.lms.controller;
 
+import com.ooops.lms.controller.BaseRowController;
 import com.ooops.lms.model.Member;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
-public class AdminUserTableRowController extends BasicUserController {
+
+public class AdminUserTableRowController extends BaseRowController<Member, AdminUserPageController> {
 
     @FXML
     private HBox mainRowHbox;
@@ -18,63 +20,11 @@ public class AdminUserTableRowController extends BasicUserController {
     @FXML
     private Label phoneNumberLabel;
 
-    private AdminUserPageController mainController;
-    private static AdminUserTableRowController selectedRow = null;
-    private Member member;
-
-    /**
-     * Hàm để set mainController (PageController).
-     *
-     * @param controller là PageController truyền vào
-     */
-    public void setMainController(AdminUserPageController controller) {
-        this.mainController = controller;
+    @Override
+    protected void updateRowDisplay() {
+        memberIDlabel.setText(String.valueOf(item.getPerson().getId()));
+        memberNameLabel.setText(item.getPerson().getFirstName() + " " + item.getPerson().getLastName());
+        phoneNumberLabel.setText(item.getPerson().getPhone());
     }
 
-    public void initialize() {
-        //Nếu người dùng click vào row thì gọi handleClickRow
-        mainRowHbox.setOnMouseClicked(event -> {
-            if (event.getClickCount() == 1) {
-                handleClickRow();
-            }
-        });
-
-    }
-
-    /**
-     * Hàm truyền member đi và load thông tin trên bảng Detail.
-     * Xử lý nếu như đang load Detail của member nào thì memberRow đấy được tô đậm.
-     */
-    public void handleClickRow() {
-        //Xử lý màu cho Row được chọn
-        if (selectedRow != null) {
-            selectedRow.resetRowColor(); // Khôi phục màu nền của hàng trước
-        }
-        selectedRow = this;
-        mainRowHbox.setStyle("-fx-background-color: #DDDCDC;");
-
-        //Load thông tin member trên bảng Detail
-        mainController.loadDetail(this.member);
-    }
-
-    /**
-     * Khôi phục lại màu nền mặc định cho row
-     */
-    public void resetRowColor() {
-        mainRowHbox.setStyle("");
-    }
-
-    /**
-     * Set member cho Row.
-     *
-     * @param member
-     */
-    public void setMember(Member member) {
-        this.member = member;
-
-        //Gán thông tin cho các trường ID,Name,SDT
-        memberIDlabel.setText(String.valueOf(member.getPerson().getId()));
-        memberNameLabel.setText(member.getPerson().getFirstName() + " " + member.getPerson().getLastName());
-        phoneNumberLabel.setText(member.getPerson().getPhone());
-    }
 }
