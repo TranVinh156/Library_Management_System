c83d0873831a3634566af870b6f3a33fac5b3963
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-19 14:16:46 +0700

    Update BookItemStatus.java

--- src/main/java/com/ooops/lms/model/enums/BookItemStatus.java
+++ src/main/java/com/ooops/lms/model/enums/BookItemStatus.java
@@ -1,8 +1,22 @@
 package com.ooops.lms.model.enums;
 
 public enum BookItemStatus {
-    AVAILABLE,
-    RESERVED,
-    LOANED,
-    LOST
+    AVAILABLE("Có sẵn"),
+    RESERVED("Đặt trước"),
+    LOANED("Đang mượn"),
+    LOST("Làm mất");
+    private final String displayName;
+
+    BookItemStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return name(); // Sử dụng tên hiển thị thay vì tên enum
+    }
 }
