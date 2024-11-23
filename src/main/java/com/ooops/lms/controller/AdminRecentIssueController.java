b316ebd6f62a8a215f2f15a91a672c9fb48b9f44
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-23 07:42:05 +0700

    Update AdminRecentIssueController.java

--- src/main/java/com/ooops/lms/controller/AdminRecentIssueController.java
+++ src/main/java/com/ooops/lms/controller/AdminRecentIssueController.java
@@ -1,6 +1,8 @@
 package com.ooops.lms.controller;
 
 import com.ooops.lms.model.Report;
+import com.ooops.lms.model.enums.ReportStatus;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -19,18 +21,33 @@ public class AdminRecentIssueController {
     private HBox mainRowHbox;
 
     private Report report;
-    private AdminIssuePageController mainController;
+    private static AdminIssuePageController mainController;
+    private static AdminMenuController adminMenuController;
 
     public void setItem(Report report) {
         this.report = report;
         idMemberLabel.setText(report.getMember().getPerson().getId()+"");
         statusLabel.setText(report.getStatus().getDisplayName());
         titleLabel.setText(report.getTitle());
+        if(report.getStatus().equals(ReportStatus.PENDING)) {
+            statusLabel.setStyle("-fx-text-fill: red;");
+        } else {
+            statusLabel.setStyle("-fx-text-fill: black;");
+        }
         setupRowClickHandler();
     }
 
     public void setMainController(AdminIssuePageController mainController) {
         this.mainController = mainController;
+        if(mainController == null) {
+            System.out.println("mainController is null");;
+        }
+    }
+    public void setMenuController(AdminMenuController menuController) {
+        this.adminMenuController = menuController;
+        if(adminMenuController == null) {
+            System.out.println("adminMenuController is null1");;
+        }
     }
     private void setupRowClickHandler() {
         mainRowHbox.setOnMouseClicked(event -> {
@@ -40,6 +57,10 @@ public class AdminRecentIssueController {
         });
     }
     protected void handleRowClick() {
+        if(adminMenuController == null) {
+            System.out.println("adminMenuController is null2");;
+        }
+        this.adminMenuController.onIssuesButtonAction(new ActionEvent());
         mainController.loadDetail(this.report);
     }
 
