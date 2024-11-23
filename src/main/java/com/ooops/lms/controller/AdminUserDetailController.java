1bfdbd73bcd30d709b2a7302a2a55c132818879a
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-24 04:16:46 +0700

    Update AdminUserDetailController.java

--- src/main/java/com/ooops/lms/controller/AdminUserDetailController.java
+++ src/main/java/com/ooops/lms/controller/AdminUserDetailController.java
@@ -4,9 +4,11 @@ import com.ooops.lms.Alter.CustomerAlter;
 import com.ooops.lms.Command.AdminCommand;
 import com.ooops.lms.Command.Command;
 import com.ooops.lms.controller.BaseDetailController;
+import com.ooops.lms.database.dao.BookIssueDAO;
 import com.ooops.lms.model.Member;
 import com.ooops.lms.model.datatype.Person;
 import com.ooops.lms.model.enums.AccountStatus;
+import com.ooops.lms.model.enums.BookIssueStatus;
 import com.ooops.lms.model.enums.Gender;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -19,6 +21,8 @@ import javafx.scene.image.ImageView;
 import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdminUserDetailController extends BaseDetailController<Member> {
     @FXML
@@ -74,7 +78,32 @@ public class AdminUserDetailController extends BaseDetailController<Member> {
         memberIDText.setText(String.valueOf(item.getPerson().getId()));
         memberNameText.setText(item.getPerson().getFirstName() + " " + item.getPerson().getLastName());
         numberOfBorrowText.setText(String.valueOf(item.getTotalBooksCheckOut()));
-        numberOfLostText.setText("Chua co");
+        try {
+            Map<String, Object> findCriteriaa = new HashMap<>();
+            findCriteriaa.put("BookIssueStatus", BookIssueStatus.BORROWED);
+            findCriteriaa.put("member_ID", item.getPerson().getId());
+
+            Map<String, Object> findCriteriaaa = new HashMap<>();
+            findCriteriaaa.put("BookIssueStatus", BookIssueStatus.RETURNED);
+            findCriteriaaa.put("member_ID", item.getPerson().getId());
+            int borrowBook = BookIssueDAO.getInstance().searchByCriteria(findCriteriaa).size() + BookIssueDAO.getInstance().searchByCriteria(findCriteriaaa).size();
+            item.setTotalBooksCheckOut(borrowBook);
+            numberOfBorrowText.setText(String.valueOf(item.getTotalBooksCheckOut()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Map<String, Object> findCriteriaa = new HashMap<>();
+            findCriteriaa.put("BookIssueStatus", BookIssueStatus.LOST);
+            findCriteriaa.put("member_ID", item.getPerson().getId());
+            int lostBook = BookIssueDAO.getInstance().searchByCriteria(findCriteriaa).size();
+            item.setTotalBooksCheckOut(lostBook);
+            numberOfLostText.setText(String.valueOf(lostBook));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         phoneNumberText.setText(item.getPerson().getPhone());
         birthText.setText(reformatDate(item.getPerson().getDateOfBirth().toString()));
         statusBox.setValue(item.getStatus());
@@ -213,6 +242,11 @@ public class AdminUserDetailController extends BaseDetailController<Member> {
 
         return true;
 
+    }
+
+    @Override
+    public String getType() {
+        return "độc giả";
     }
 
     @FXML
