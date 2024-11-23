2531c944e7be04dce84ef778083cd67c950b30c0
chie41 <91408610+chie41@users.noreply.github.com>
2024-11-23 23:07:20 +0700

    Update AdminReservationTableController.java

--- src/main/java/com/ooops/lms/controller/AdminReservationTableController.java
+++ src/main/java/com/ooops/lms/controller/AdminReservationTableController.java
@@ -4,16 +4,21 @@ import com.ooops.lms.database.dao.BookIssueDAO;
 import com.ooops.lms.database.dao.BookReservationDAO;
 import com.ooops.lms.model.BookIssue;
 import com.ooops.lms.model.BookReservation;
+import com.ooops.lms.model.enums.BookReservationStatus;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-public class AdminReservationTableController extends BaseTableController<BookReservation, AdminReservationPageController,AdminReservationTableRowController> {
+public class AdminReservationTableController extends BaseTableController<BookReservation, AdminReservationPageController, AdminReservationTableRowController> {
 
     private static final String ROW_FXML = "/com/ooops/lms/library_management_system/AdminReservationTableRow.fxml";
 
@@ -39,13 +44,19 @@ public class AdminReservationTableController extends BaseTableController<BookRes
     private ScrollPane scrollPane;
 
     @FXML
-    private TextField statusFindText;
+    private ChoiceBox<String> statusFindBox;
 
     @FXML
     private Button findButton;
 
     @FXML
     private VBox tableVbox;
+
+    @FXML
+    protected void initialize() {
+        statusFindBox.getItems().add("None");
+        statusFindBox.getItems().addAll(BookReservationStatus.CANCELED.toString(), BookReservationStatus.WAITING.toString(), BookReservationStatus.COMPLETED.toString());
+    }
 
     @Override
     protected String getRowFXML() {
@@ -56,27 +67,35 @@ public class AdminReservationTableController extends BaseTableController<BookRes
     protected void loadDataFromSource() throws SQLException {
         itemsList.addAll(BookReservationDAO.getInstance().selectAll());
     }
+
     @Override
-    protected void getCriteria(){
+    protected void getCriteria() {
         findCriteria.clear();
-        if(!barCodeFindText.getText().isEmpty()){
-            findCriteria.put("barCode",barCodeFindText.getText());
+        if (!barCodeFindText.getText().isEmpty()) {
+            findCriteria.put("barcode", barCodeFindText.getText());
         }
         if(!bookNameFindText.getText().isEmpty()){
-            findCriteria.put("bookName",bookNameFindText.getText());
+            findCriteria.put("title",bookNameFindText.getText());
         }
-        if(!borrowDateFindText.getText().isEmpty()){
-            findCriteria.put("borrowDate",borrowDateFindText.getText());
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // hoặc định dạng phù hợp với dữ liệu của bạn
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        // Chuyển đổi và định dạng cho borrowDate
+        if (borrowDateFindText.getText() != null && !borrowDateFindText.getText().isEmpty()) {
+            LocalDate createdDate = LocalDate.parse(borrowDateFindText.getText(), inputFormatter);
+            createdDate.format(outputFormatter);
+            findCriteria.put("creation_date", createdDate.toString());
         }
-        if(!borrowerFindText.getText().isEmpty()){
-            findCriteria.put("borrower",borrowerFindText.getText());
+
+        if (!borrowerFindText.getText().isEmpty()) {
+            findCriteria.put("fullname", borrowerFindText.getText());
         }
-        if(!memeberIDFindText.getText().isEmpty()){
-            findCriteria.put("memeberID",memeberIDFindText.getText());
+        if (!memeberIDFindText.getText().isEmpty()) {
+            findCriteria.put("member_ID", memeberIDFindText.getText());
         }
-        if(!statusFindText.getText().isEmpty()){
-            findCriteria.put("status",statusFindText.getText());
+        if (!statusFindBox.getItems().isEmpty() && statusFindBox.getValue() != "None" && statusFindBox.getValue() != null) {
+            findCriteria.put("BookReservationStatus", statusFindBox.getValue());
         }
+
     }
 
     @FXML
@@ -85,8 +104,24 @@ public class AdminReservationTableController extends BaseTableController<BookRes
     }
 
     @FXML
-    void onFindButtonAction(ActionEvent event) {
+    protected void searchCriteria() {
+        getCriteria();
+        if (findCriteria.isEmpty()) {
+            loadData();
+            return;
+        }
+        try {
+            itemsList.clear();
+            itemsList.addAll(BookReservationDAO.getInstance().searchByCriteria(findCriteria));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        loadRows();
+    }
 
+    @FXML
+    void onFindButtonAction(ActionEvent event) {
+        searchCriteria();
     }
 
 }
