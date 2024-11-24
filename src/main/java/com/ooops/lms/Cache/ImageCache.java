500e6350cd56564eceb0ba6d83a1fbaf10cde8e1
TranVinh156 <94176722+TranVinh156@users.noreply.github.com>
2024-11-25 01:51:41 +0700

    Update ImageCache.java

--- src/main/java/com/ooops/lms/Cache/ImageCache.java
+++ src/main/java/com/ooops/lms/Cache/ImageCache.java
@@ -4,7 +4,7 @@ import com.mysql.cj.util.LRUCache;
 import javafx.scene.image.Image;
 
 public class ImageCache {
-    private static LRUCache<String, Image> imageLRUCache = new LRUCache(100);
+    private static LRUCache<String, Image> imageLRUCache = new LRUCache(200);
 
     public static LRUCache<String, Image> getImageLRUCache() {
         return imageLRUCache;
